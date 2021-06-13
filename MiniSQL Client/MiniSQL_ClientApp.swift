80413f609697e663f57e0aeaa4b8c4c76739e6fe
Yuanlin <im.yuanlinLin@gmail.com>
2021-06-13 14:35:29 +0800

    Initial Commit

---
+++ MiniSQL Client/MiniSQL_ClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniSQL_ClientApp.swift
+//  MiniSQL Client
+//
+//  Created by Yuanlin Lin on 2021/6/13.
+//
+
+import SwiftUI
+
+@main
+struct MiniSQL_ClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
